6e618b3ed80937cea82608ee287332d42716ad73
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-05-07 05:42:03 +0300

    feat: чисÑrefactor° проекта

---
+++ src/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #111111;
+  color: #eeeeee;
+  font-family: "Roboto";
+  margin: 0;
+}
+
+</style>
