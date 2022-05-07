dbb7f8b9f01a0d47eb1c93b765eb6373a6673631
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-05-07 12:11:07 +0300

    feat: вестка блока отзыва

--- src/pages/FilmPage.vue
+++ src/pages/FilmPage.vue
@@ -7,25 +7,42 @@
         :style="isEmpty && {height: 'calc(100% - 90px)'}"
         class="film-page__image"
     >
-    <p v-if="!isEmpty" class="film-page__content-title">Немного о сюжете:</p>
-    <p v-if="!isEmpty" class="film-page__content-text">{{ film.plot }}</p>
-    <p v-if="!isEmpty" class="film-page__content-title">Рейтинг:</p>
-    <p v-if="!isEmpty" class="film-page__content-text">{{ film.rating }}</p>
-    <p v-if="!isEmpty" class="film-page__content-title">Актерский состав:</p>
-    <p v-if="!isEmpty" class="film-page__content-text">{{ film.actors }}</p>
-    <p v-if="!isEmpty" class="film-page__content-title">Режиссеры:</p>
-    <p v-if="!isEmpty" class="film-page__content-text">{{ film.directors }}</p>
+    <div class="film-page__description-box">
+      <p v-if="!isEmpty" class="film-page__content-title">Немного о сюжете:</p>
+      <p v-if="!isEmpty" class="film-page__content-text">{{ film.plot }}</p>
+      <p v-if="!isEmpty" class="film-page__content-title">Рейтинг:</p>
+      <p v-if="!isEmpty" class="film-page__content-text">{{ film.rating }}</p>
+      <p v-if="!isEmpty" class="film-page__content-title">Актерский состав:</p>
+      <p v-if="!isEmpty" class="film-page__content-text">{{ film.actors }}</p>
+      <p v-if="!isEmpty" class="film-page__content-title">Режиссеры:</p>
+      <p v-if="!isEmpty" class="film-page__content-text">{{ film.directors }}</p>
+    </div>
+    <div class="film-page__review-box">
+      <p class="film-page__review-title">Смотрели данный фильм?</p>
+      <p class="film-page__review-text">Оставьте отзыв:</p>
+      <ul class="film-page__review-list">
+        <li
+            v-for="reaction in reactions"
+            :key="reaction.id"
+            class="button film-page__review-item"
+        >
+          {{reaction.title}}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import films from "@/utils/films";
+import reactions from "@/utils/reactions";
 
 export default {
   data() {
     return {
       film: null,
-      isEmpty: false
+      isEmpty: false,
+      reactions
     }
   },
   created() {
@@ -56,16 +73,6 @@ export default {
   margin-bottom: 30px;
 }
 
-.film-page__title::after {
-  content: '';
-  position: absolute;
-  height: 2px;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: linear-gradient(90deg, #e15353 0%, #e1535300 80%);
-}
-
 .film-page__image {
   margin-bottom: 25px;
   object-fit: cover;
@@ -83,5 +90,43 @@ export default {
 
 .film-page p:not(:last-of-type) {
   margin-bottom: 10px;
+}
+
+.film-page__description-box {
+  position: relative;
+  padding-bottom: 15px;
+}
+
+.film-page__title::after,
+.film-page__description-box::after {
+  content: '';
+  position: absolute;
+  height: 2px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: linear-gradient(90deg, #e15353 0%, #e1535300 80%);
+}
+
+.film-page__review-box {
+  padding-top: 30px;
+}
+
+.film-page__review-title {
+  font-size: 24px;
+}
+
+.film-page__review-text {
+  margin-bottom: 15px;
+  color: #e15353;
+}
+
+.film-page__review-list {
+  display: flex;
+  gap: 15px;
+}
+
+.film-page__review-item {
+
 }
 </style>
