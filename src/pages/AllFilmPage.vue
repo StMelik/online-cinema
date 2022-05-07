b7229bb109fe4799caae81e65b63cc91f814ba40
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-05-07 07:24:50 +0300

    refactor: изменить ховер и вынести данные в отдельный файл

--- src/pages/AllFilmPage.vue
+++ src/pages/AllFilmPage.vue
@@ -8,6 +8,7 @@
             v-for="film in films"
             :key="film.id"
         >
+          <p class="item-films__more">Подробнее о фильме</p>
           <img class="item-films__image" :src="film.imageUrl" :alt="film.title">
           <div class="item-films__rating">{{ film.rating }}</div>
           <p class="item-films__title">{{ film.title }}</p>
@@ -18,47 +19,12 @@
 </template>
 
 <script>
+import {films} from "@/utils/films";
+
 export default {
   data() {
     return {
-      films: [
-        {
-          id: 1,
-          imageUrl: 'https://api.kinoart.ru/storage/post/1492/regular_detail_picture-542c58ab285d5c980aab93d222acdede.jpg',
-          rating: 8.5,
-          title: 'Джентельмены'
-        },
-        {
-          id: 2,
-          imageUrl: 'https://itc.ua/wp-content/uploads/2021/12/3-6.jpg',
-          rating: 8.1,
-          title: 'Человек-паук 3: Нет пути домой'
-        },
-        {
-          id: 3,
-          imageUrl: 'https://thumbs.dfs.ivi.ru/storage26/contents/1/0/4eab3e3f265eb29505f5d643b966d8.jpg',
-          rating: 7.8,
-          title: 'Круэлла'
-        },
-        {
-          id: 4,
-          imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbK3279sFpD7JVLLe-Eg-wudGMLGf7t_nlSQ&usqp=CAU',
-          rating: 8.8,
-          title: 'Железный человек 2'
-        },
-        {
-          id: 5,
-          imageUrl: 'https://s.afisha.ru/mediastorage/51/99/e890934d66fa49eba4579d359951.jpg',
-          rating: 7.5,
-          title: 'Время (2011)'
-        },
-        {
-          id: 6,
-          imageUrl: 'https://kto-ozvuchivaet.ru/wp-content/uploads/2021/08/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B3%D0%B5%D1%80%D0%BE%D0%B9-%D1%84%D0%B8%D0%BB%D1%8C%D0%BC-2021.jpg',
-          rating: 8.5,
-          title: 'Главный герой'
-        },
-      ]
+      films
     }
   }
 }
@@ -85,11 +51,24 @@ export default {
   border-radius: 15px;
   overflow: hidden;
   cursor: pointer;
+}
+
+.item-films__more {
+  position: absolute;
+  display: flex;
+  visibility: hidden;
+  opacity: 0;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(17, 17, 17, 0.5);
   transition: all .3s;
 }
 
-.item-films:hover {
-  transform: scale(1.03);
+.item-films:hover .item-films__more {
+  visibility: visible;
+  opacity: 1;
 }
 
 .item-films__image {
